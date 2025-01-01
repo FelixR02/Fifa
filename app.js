34e101c904f536879f5986868766ed3bf02b9603
FelixR02 <rodriguezutrerafelixalberto02@gmail.com>
2025-01-01 17:00:01 -0500

    el swagger para players funciona

--- app.js
+++ app.js
@@ -1,39 +1,58 @@
-const express = require("express")
-const app = express()
-const playersRoutes = require("./rutas/playersRoutes.js")
-const teamsRoutes = require("./rutas/teamsRoutes.js")
-const tournamentsRoutes = require("./rutas/tournamentsRoutes.js")
+const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+const app = express();
+const playersRoutes = require("./rutas/playersRoutes.js");
+const teamsRoutes = require("./rutas/teamsRoutes.js");
+const tournamentsRoutes = require("./rutas/tournamentsRoutes.js");
 
-app.use(express.json())
+app.use(express.json());
 
-//Swagger UI
+// Configuración de Swagger
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: "3.0.0",
+        info: {
+            title: "API FutbolStats",
+            version: "1.0.0",
+            description: "Documentación de la API",
+        },
+        servers: [
+            {
+                url: "http://localhost:3000",
+            },
+        ],
+    },
+    apis: ["./rutas/*.js"], // Rutas donde se encuentran las definiciones de las API
+};
+
+const swaggerSpec = swaggerJsDoc(swaggerOptions);
+
+// Swagger UI
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-//rutas
-app.use('/players', playersRoutes)
-app.use('/teams', teamsRoutes)
-app.use('/tournaments',tournamentsRoutes)
+// Rutas
+app.use('/players', playersRoutes);
+app.use('/teams', teamsRoutes);
+app.use('/tournaments', tournamentsRoutes);
 
-app.get("/",(req,res) =>{
-    res.send("Hola, mundo")
-})
+app.get("/", (req, res) => {
+    res.send("Hola, mundo");
+});
 
-app.listen(3000,()=>{
-    console.log("Servidor iniciado en el puerto 3000")
-
-})
-
+app.listen(3000, () => {
+    console.log("Servidor iniciado en el puerto 3000");
+});
 
 // Instancia de Sequelize para conectarse a la base de datos
-const sequelize = require("./helpers/database.js"); 
-
-
+const sequelize = require("./helpers/database.js");
 
 // Sincronizar los modelos para verificar la conexión con la base de datos
- sequelize
- .sync({ alter: true })
- .then(() => {
- console.log("Todos los modelos se sincronizaron correctamente.");
- }) .catch((err) => {
- console.log("Ha ocurrido un error al sincronizar los modelos: ", err); 
-});
+sequelize
+    .sync({ alter: true })
+    .then(() => {
+        console.log("Todos los modelos se sincronizaron correctamente.");
+    })
+    .catch((err) => {
+        console.log("Ha ocurrido un error al sincronizar los modelos: ", err);
+    });
