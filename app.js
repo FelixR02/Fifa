d5e07907b4a11b0af3fb8f8b439a835ee362f9c8
FelixR02 <rodriguezutrerafelixalberto02@gmail.com>
2025-02-07 18:14:56 -0500

    swagger render

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const swaggerOptions = {
         },
         servers: [
             {
-                url: "http://localhost:3000",
+                url: "http://localhost:3000", "https://fifa-u40o.onrender.com"
             },
         ],
         components: {
